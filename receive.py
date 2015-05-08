18708d47bea0298ab52912cbdfcfaedcd0794e42
David Barron <ivandavid77@gmail.com>
2015-05-08 10:44:02 -0500

    fix typo error

--- receive.py
+++ receive.py
@@ -9,4 +9,4 @@ channel = connection.channel()
 channel.queue_declare(queue='hello')
 channel.basic_consume(callback, queue='hello',no_ack=True)
 print ' [*] Waiting for messages. To exit press CTRL+C'
-channel.start_consumming()
+channel.start_consuming()
