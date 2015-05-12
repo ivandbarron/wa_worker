a1e73e0734a9f0adecf12b9fb404732c8047db60
Ivan David Barron Baez <ivandavid77@gmail.com>
2015-05-11 20:04:04 -0500

    pip install was correctly verified

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export ETCD_ENDPOINT="$(ifconfig docker0 | awk '/\<inet\>/ { print $2}')"
-#pip install -r /mnt/wa_worker/requirements.txt
+pip install -r /mnt/wa_worker/requirements.txt
 while true; do
     echo running;
     sleep 1;
