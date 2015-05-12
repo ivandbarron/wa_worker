8ab05064041a5834a9dadb572779b4fff349bfb5
David Barron <ivandavid77@gmail.com>
2015-05-12 18:40:49 -0500

    enable full stack trace for debugging

--- message_receiver/start.py
+++ message_receiver/start.py
@@ -35,24 +35,24 @@ if __name__ == '__main__':
     init_search_path()
     init_logger()
     log = logging.getLogger(__name__)
-    while True:
-        host = 'NULL'
-        port = 0
-        queue = 'NULL'
-        try:
-            host, port, queue = get_mq_params()
-            log.info('Trying to connect...')
-            receive.from_queue(host, port, queue)
-            break
-        except Exception as e:
-            log.warn('Error with config: %s:%s %r, retry in 40 seconds: %r' %
-                     (host, port, queue, str(e)))
-            time.sleep(30)
-            '''TODO: replication
-            log.warn('Error with config: %s:%s %r' % (host, port, queue))
-            if get_slave:
-                log.error('Error connecting to slave! Retry in 30 seconds')
-                time.sleep(30)
-            else:
-                get_slave = True
-            '''
+    #while True:
+        #host = 'NULL'
+        #port = 0
+        #queue = 'NULL'
+        #try:
+    host, port, queue = get_mq_params()
+    log.info('Trying to connect...')
+    receive.from_queue(host, port, queue)
+        #break
+        #except Exception as e:
+        #    log.warn('Error with config: %s:%s %r, retry in 40 seconds: %r' %
+        #             (host, port, queue, str(e)))
+        #    time.sleep(30)
+        #    '''TODO: replication
+        #    log.warn('Error with config: %s:%s %r' % (host, port, queue))
+        #    if get_slave:
+        #        log.error('Error connecting to slave! Retry in 30 seconds')
+        #        time.sleep(30)
+        #    else:
+        #        get_slave = True
+        #    '''
