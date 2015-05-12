abfc52e4ac3560aa6971737bae30b22253e8f0d5
Ivan David Barron Baez <ivandavid77@gmail.com>
2015-05-11 20:09:08 -0500

    pending test export

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export ETCD_ENDPOINT="$(ifconfig docker0 | awk '/\<inet\>/ { print $2}')"
+#export ETCD_ENDPOINT="$(ifconfig docker0 | awk '/\<inet\>/ { print $2}')"
 pip install -r /mnt/wa_worker/requirements.txt
 while true; do
     echo running;
