f8064c1e2eaf379e6fdb7fb0f187f2e3573b7b44
Iván David Barrón Báez <ivandavid77@gmail.com>
2015-05-13 15:49:24 -0500

    added example and fixed multiline condition error

--- debug/send_message.py
+++ debug/send_message.py
@@ -15,15 +15,23 @@ else:
     msg = sys.argv[1]
     try:
         _json = json.loads(msg)
-        if not 'phones' in _json or
-            not 'mails' in _json or
-            not 'msg' in _json or
-            not type(_json['phones']) == list or
-            not type(_json['mails']) == list or
+        if not 'phones' in _json or \
+            not 'mails' in _json or \
+            not 'msg' in _json or \
+            not type(_json['phones']) == list or \
+            not type(_json['mails']) == list or \
             not type(_json['msg']) == str:
             raise Exception()
     except:
-        print('Invalid format for message')
+        usage = '''
+            Invalid json format, try with the next example:
+            {
+                "phones": ["5212287779788", "5212287779788"],
+                "mails": ["dbarron@crediland.com.mx", "ivandavid77@gmail.com"],
+                "msg": "test with newline#13test again"
+            }
+        '''
+        print(usage)
         sys.exit(1)
 etcd_endpoint = os.getenv('ETCD_ENDPOINT', '127.0.0.1')
 etcd_port = int(os.getenv('ETCD_PORT', '4001'))
