03c57b2ac433ce72643e9530016bdfb11147fb73
Iván David Barrón Báez <ivandavid77@gmail.com>
2015-05-19 18:21:49 -0500

    fix malformed json

--- wa_worker/scheduler/utilities/queries/venta.dia_semana.sql
+++ wa_worker/scheduler/utilities/queries/venta.dia_semana.sql
@@ -7,7 +7,7 @@ SELECT
     v.fecha_venta AS dia,
     HOUR(v.hora) AS hora,
     COUNT(v.folio_venta) AS ope,
-    SUM(v.valor_venta/(IF(v.clave_muebleria = "TC18" AND v.fecha_venta < "2014-01-01","1.11","1.16"))) AS vta
+    SUM(v.valor_venta/(IF(v.clave_muebleria = 'TC18' AND v.fecha_venta < '2014-01-01','1.11','1.16'))) AS vta
 FROM
     ventas AS v
     INNER JOIN mueblerias AS m ON
@@ -15,7 +15,7 @@ FROM
 WHERE
     v.fecha_venta = @FECHA_ACTUAL AND
     v.valor_venta > 0 AND
-    v.tipo_venta <> "0A" AND
+    v.tipo_venta <> '0A' AND
     #FILTROS AND
     HOUR(v.hora) < HOUR(@HORA_ACTUAL) AND
     m.activa = 1
@@ -26,7 +26,7 @@ SELECT
     b.fecha AS dia,
     HOUR(v.hora) AS hora,
     0-COUNT(b.folio_venta) AS ope,
-    0-SUM(v.valor_venta/(IF(v.clave_muebleria = "TC18" AND v.fecha_venta < "2014-01-01","1.11","1.16"))) AS vta
+    0-SUM(v.valor_venta/(IF(v.clave_muebleria = 'TC18' AND v.fecha_venta < '2014-01-01','1.11','1.16'))) AS vta
 FROM
     baja AS b
     INNER JOIN ventas AS v ON
@@ -35,7 +35,7 @@ FROM
         m.clave_muebleria = v.clave_muebleria
 WHERE
     b.fecha = @FECHA_ACTUAL AND
-    b.causa LIKE "C%" AND
+    b.causa LIKE 'C%' AND
     #FILTROS AND
     HOUR(v.hora) < HOUR(@HORA_ACTUAL) AND
     m.activa = 1
@@ -46,7 +46,7 @@ SELECT
     v.fecha_venta AS dia,
     HOUR(v.hora) AS hora,
     COUNT(v.folio_venta) AS ope,
-    SUM(v.valor_venta/(IF(v.clave_muebleria = "TC18" AND v.fecha_venta < "2014-01-01","1.11","1.16"))) AS vta
+    SUM(v.valor_venta/(IF(v.clave_muebleria = 'TC18' AND v.fecha_venta < '2014-01-01','1.11','1.16'))) AS vta
 FROM
     ventas AS v
     INNER JOIN mueblerias AS m ON
@@ -54,7 +54,7 @@ FROM
 WHERE
     v.fecha_venta = @FECHA_ANTERIOR AND
     v.valor_venta > 0 AND
-    v.tipo_venta <> "0A" AND
+    v.tipo_venta <> '0A' AND
     #FILTROS AND
     HOUR(v.hora) < HOUR(@HORA_ACTUAL) AND
     m.activa = 1
@@ -65,7 +65,7 @@ SELECT
     b.fecha AS dia,
     HOUR(v.hora) AS hora,
     0-COUNT(b.folio_venta) AS ope,
-    0-SUM(v.valor_venta/(IF(v.clave_muebleria = "TC18" AND v.fecha_venta < "2014-01-01","1.11","1.16"))) AS vta
+    0-SUM(v.valor_venta/(IF(v.clave_muebleria = 'TC18' AND v.fecha_venta < '2014-01-01','1.11','1.16'))) AS vta
 FROM
     baja AS b
     INNER JOIN ventas AS v ON
@@ -74,7 +74,7 @@ FROM
         m.clave_muebleria = v.clave_muebleria
 WHERE
     b.fecha = @FECHA_ANTERIOR AND
-    b.causa LIKE "C%" AND
+    b.causa LIKE 'C%' AND
     #FILTROS AND
     HOUR(v.hora) < HOUR(@HORA_ACTUAL) AND
     m.activa = 1
