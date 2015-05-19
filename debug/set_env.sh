a2f34cd991c7c5a2a75418b3857a8e6a21807a81
Iván David Barrón Báez <ivandavid77@gmail.com>
2015-05-19 18:10:07 -0500

    added MQ_TASK_MANAGEMENT_QUEUE for debug

--- debug/set_env.sh
+++ debug/set_env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Don't forget setting with: $ source ./set_env.sh
 export MQ_QUEUE=WA_MESSAGE_QUEUE
+export MQ_TASK_MANAGEMENT_QUEUE=WA_MANAGEMENT_QUEUE
 export SECRET_KEY=MjAxNTA1MTMxNTE3
 export ETCD_ENDPOINT=172.16.201.204
 export ETCD_PORT=4001
