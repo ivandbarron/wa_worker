5ad91552fcc4e169fbc8f2706831a4d86b0d1a2a
Iván David Barrón Báez <ivandavid77@gmail.com>
2015-05-19 16:58:48 -0500

    files to correct folder

--- wa_worker/scheduler/utilities/add_task.sh
+++ wa_worker/scheduler/utilities/add_task.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-python send_message.py \
---name "venta_por_hora_todos" \
+python add_task.py \
+--name "venta_david" \
 --phones 5212287779788
 --emails dbarron@crediland.com.mx
---cron 0 9-21/1 * * 1-6
---sql_file queries/venta.dia_semana.sql
+--cron */5 * * * *
+--sql_file $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/queries/venta.dia_semana.sql
 --params \
     @FECHA_ACTUAL=CURDATE() \
     "@FECHA_ANTERIOR=DATE_SUB(@FECHA_ACTUAL,INTERVAL 364 DAY)" \
