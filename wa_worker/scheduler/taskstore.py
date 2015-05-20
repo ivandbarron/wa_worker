442e35e38ea56a1b0624feeb6df3ff84de001474
Iván David Barrón Báez <ivandavid77@gmail.com>
2015-05-20 10:16:21 -0500

    cron is not a list anymore

--- wa_worker/scheduler/taskstore.py
+++ wa_worker/scheduler/taskstore.py
@@ -42,7 +42,7 @@ def add_task(name, cron, phones, emails, sql):
     crond = CronTab(user=True)
     job = crond.new(command='/usr/bin/python '+__file__+' --task_name '+name)
     job.set_comment(name)
-    job.setall(' '.join(cron))
+    job.setall(cron)
     crond.write_to_user(user=True)
 
 
