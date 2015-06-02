f9aa128735e812cc55d6c3626ac987537896a38c
Iván David Barrón Báez <ivandavid77@gmail.com>
2015-06-02 10:51:37 -0500

    added syslog facility

--- start.sh
+++ start.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
-echo ' [*] Modify timezone to Mexico City...';
+echo -e ' [*] Modify timezone to Mexico City...\n';
 rm /etc/localtime
 ln -s /usr/share/zoneinfo/America/Mexico_City /etc/localtime
-echo ''
-echo ' [*] Saving current environment vars'
+echo -e ' [*] Saving current environment vars...\n'
 # Used by wa_worker/wa_worker/scheduler/taskstore.sh (triggered by crond)
 echo '#!/usr/bin/bash' > /tmp/set_environment_vars.sh
 chmod 0755 /tmp/set_environment_vars.sh
 env > /tmp/environment_vars
 sed -e 's/^/export\ /' /tmp/environment_vars >> /tmp/set_environment_vars.sh
 rm /tmp/environment_vars
-echo ''
-echo ' [*] Starting crond...'
+echo -e ' [*] Configuring and starting rsyslogd...\n'
+cat /etc/rsyslog.conf | sed 's/\$ModLoad\ imjournal/\#$ModLoad\ imjournal/g' | sed 's/\$OmitLocalLogging on/\$OmitLocalLogging\ off/g' | sed 's/\$IMJournalStateFile/\#\$IMJournalStateFile/g' > /etc/rsyslog.conf
+echo '' >> /etc/rsyslog.conf
+cat /etc/rsyslog.d/listen.conf | sed 's/\$SystemLogSocketName/\#\$SystemLogSocketName/g' > /etc/rsyslog.d/listen.conf
+rsyslogd
+echo -e ' [*] Starting crond...\n'
 crond -p
-echo '';
-echo ' [*] Loading requirements...';
+echo -e ' [*] Loading requirements...\n';
 pip install -r $MOUNT_POINT/wa_worker/requirements.txt
-echo '';
-echo ' [*] Starting message receiver...';
+echo -e ' [*] Starting message receiver...\n';
 python $MOUNT_POINT/wa_worker/wa_worker/message_receiver/start.py &
-echo '';
-echo ' [*] Starting scheduler...';
+echo -e ' [*] Starting scheduler...\n';
 python $MOUNT_POINT/wa_worker/wa_worker/scheduler/start.py &
-echo ''
-echo ' [*] Adding default task...'
+echo -e ' [*] Adding default task...\n'
 $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/add_venta.sh
 $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/add_cierre.sh
 while true; do
