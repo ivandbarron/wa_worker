f0b96d18cfd96f9b67eb3df688437e5e9b4de602
Ivan David Barron Baez <ivandavid77@gmail.com>
2015-10-01 13:29:43 -0500

    added caravana 2015

---
+++ wa_worker/scheduler/utilities/add_venta_caravana.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Venta dia semana de lunes a domingo
+python $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/task_manager.py \
+--op add \
+--name venta_semana_caravana \
+--phones 5212289790978 5212281404251 5212281049275 \
+--mails hrodriguez@crediland.com.mx julio.tovar@crediland.com.mx pmarin@crediland.com.mx \
+--cron "5 11-23 * * 0-6" \
+--sql_file $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/queries/venta.sql \
+--params \
+    "@FECHA_ACTUAL=CURDATE()" \
+    "@FECHA_ANTERIOR=DATE_SUB(@FECHA_ACTUAL,INTERVAL 384 DAY)" \
+    "@LEYENDA=CONCAT('Del mismo dia en la semana ',WEEK(@FECHA_ACTUAL,6),' del anio')" \
+    "#FILTROS=v.clave_muebleria = 'TC99'"
+
+
+# Venta dia calendario de lunes a domingo
+python $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/task_manager.py \
+--op add \
+--name venta_calendario_caravana \
+--phones 5212289790978 5212281404251 5212281049275 \
+--mails hrodriguez@crediland.com.mx julio.tovar@crediland.com.mx pmarin@crediland.com.mx \
+--cron "6 11-23 * * 0-6" \
+--sql_file $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/queries/venta.sql \
+--params \
+    "@FECHA_ACTUAL=CURDATE()" \
+    "@FECHA_ANTERIOR=DATE_SUB(@FECHA_ACTUAL,INTERVAL 385 DAY)" \
+    "@LEYENDA='Del mismo dia del calendario'" \
+    "#FILTROS=v.clave_muebleria = 'TC99'"
+
