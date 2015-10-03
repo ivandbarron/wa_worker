5f4717cf8e49a8f76a3c6472039c7afc269f0693
Ivan David Barron Baez <ivandavid77@gmail.com>
2015-10-03 08:51:59 -0500

    changed task

--- wa_worker/scheduler/utilities/add_cierre_caravana.sh
+++ wa_worker/scheduler/utilities/add_cierre_caravana.sh
@@ -6,12 +6,12 @@ python $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/task_manager.py \
 --name cierre_semana_caravana \
 --phones 5212289790978 5212281404251 5212281049275 \
 --mails hrodriguez@crediland.com.mx julio.tovar@crediland.com.mx pmarin@crediland.com.mx \
---cron "10 23 * * 0-6" \
+--cron "49 23 * * 0-6" \
 --sql_file $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/queries/cierre.sql \
 --params \
     "@FECHA_ACTUAL=CURDATE()" \
     "@FECHA_ANTERIOR=DATE_SUB(@FECHA_ACTUAL,INTERVAL 385 DAY)" \
-    "@LEYENDA='CARAVANA VIERNES 2014 vs 2015'" \
+    "@LEYENDA='CARAVANA SABADO 2014 vs 2015'" \
     "#FILTROS=v.clave_muebleria = 'TC99'"
 
 # Venta dia calendario de lunes a domingo
@@ -20,11 +20,11 @@ python $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/task_manager.py \
 --name cierre_calendario_caravana \
 --phones 5212289790978 5212281404251 5212281049275 \
 --mails hrodriguez@crediland.com.mx julio.tovar@crediland.com.mx pmarin@crediland.com.mx \
---cron "11 23 * * 0-6" \
+--cron "50 23 * * 0-6" \
 --sql_file $MOUNT_POINT/wa_worker/wa_worker/scheduler/utilities/queries/cierre.sql \
 --params \
     "@FECHA_ACTUAL=CURDATE()" \
     "@FECHA_ANTERIOR=DATE_SUB(@FECHA_ACTUAL,INTERVAL 384 DAY)" \
-    "@LEYENDA='CARAVANA 13/SEP/2014 vs 2/OCT/2015'" \
+    "@LEYENDA='CARAVANA 14/SEP/2014 vs 3/OCT/2015'" \
     "#FILTROS=v.clave_muebleria = 'TC99'"
 
