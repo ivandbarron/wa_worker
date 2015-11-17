192b05f47aa4b95c493b1ef562792969c6e15d5a
Iván David Barrón Báez <ivandavid77@gmail.com>
2015-11-16 22:40:30 -0600

    minor changes

--- wa_worker/scheduler/utilities/queries/flujo_efectivo.sql
+++ wa_worker/scheduler/utilities/queries/flujo_efectivo.sql
@@ -31,7 +31,7 @@ FROM
     ventas AS v
     LEFT JOIN credito AS c ON
         c.folio_venta = v.folio_venta
-WHERE v.fecha_venta = @FECHA_ACTUAL;
+WHERE v.fecha_venta = @FECHA_ACTUAL AND HOUR(v.hora) < HOUR(@HORA_ACTUAL);
 
 INSERT INTO tmp_ingresos_dia
 SELECT
@@ -52,7 +52,7 @@ FROM
         v.folio_venta = b.folio_venta
 WHERE
     b.fecha = @FECHA_ACTUAL AND
-    b.causa <> 'A1';
+    b.causa <> 'A1'  AND HOUR(v.hora) < HOUR(@HORA_ACTUAL);
 
 INSERT INTO tmp_ingresos_dia
 SELECT
@@ -77,24 +77,24 @@ WHERE
     a.fecha_abono = @FECHA_ACTUAL AND
     a.punto_recepcion NOT LIKE 'CCI%' AND
     a.punto_recepcion <> 'CD02' AND
-    v.clave_muebleria LIKE 'TC%';
+    v.clave_muebleria LIKE 'TC%' ;
 
 SELECT
     CONCAT_WS(
         CHAR(10 USING utf8),
         @LEYENDA,
         CONCAT('A las ',HOUR(@HORA_ACTUAL),' horas'),
-        CONCAT('Contado efectivo $ ',FORMAT(IFNULL(SUM(CE),0.0),2)),
-        CONCAT('Contado tarjeta $ ',FORMAT(IFNULL(SUM(CT),0.0),2)),
-        CONCAT('Contado resto $ ',FORMAT(IFNULL(SUM(CR),0.0),2)),
-        CONCAT('Enganches $ ',FORMAT(IFNULL(SUM(ENG),0.0),2)),
-        CONCAT('Cobros tienda $ ',FORMAT(IFNULL(SUM(ABOT),0.0),2)),
-        CONCAT('Cobros telefonica $ ',FORMAT(IFNULL(SUM(ABOG),0.0),2)),
-        CONCAT('Cobros presencial $ ',FORMAT(IFNULL(SUM(ABOM),0.0),2)),
-        CONCAT('Cobros Extra-Jud. $ ',FORMAT(IFNULL(SUM(ABOE),0.0),2)),
-        CONCAT('Cobros Pre-Jud. $ ',FORMAT(IFNULL(SUM(ABOP),0.0),2)),
-        CONCAT('Cobros credinomina $ ',FORMAT(IFNULL(SUM(ABOI),0.0),2)),
-        CONCAT('Cobros cart. dict. $ ',FORMAT(IFNULL(SUM(OTR),0.0),2)),
+        CONCAT('Contado efectivo $ ',FORMAT(IFNULL(SUM(CE),0.0)/1.16,2)),
+        CONCAT('Contado tarjeta $ ',FORMAT(IFNULL(SUM(CT),0.0)/1.16,2)),
+        CONCAT('Contado resto $ ',FORMAT(IFNULL(SUM(CR),0.0)/1.16,2)),
+        CONCAT('Enganches $ ',FORMAT(IFNULL(SUM(ENG),0.0)/1.16,2)),
+        CONCAT('Cobros tienda $ ',FORMAT(IFNULL(SUM(ABOT),0.0)/1.16,2)),
+        CONCAT('Cobros telefonica $ ',FORMAT(IFNULL(SUM(ABOG),0.0)/1.16,2)),
+        CONCAT('Cobros presencial $ ',FORMAT(IFNULL(SUM(ABOM),0.0)/1.16,2)),
+        CONCAT('Cobros Extra-Jud. $ ',FORMAT(IFNULL(SUM(ABOE),0.0)/1.16,2)),
+        CONCAT('Cobros Pre-Jud. $ ',FORMAT(IFNULL(SUM(ABOP),0.0)/1.16,2)),
+        CONCAT('Cobros credinomina $ ',FORMAT(IFNULL(SUM(ABOI),0.0)/1.16,2)),
+        CONCAT('Cobros cart. dict. $ ',FORMAT(IFNULL(SUM(OTR),0.0)/1.16,2)),
                       '------------------------------------',
-        CONCAT('TOTAL:  $ ',FORMAT(IFNULL(SUM(CE+CT+CR+ENG+ABOT+ABOG+ABOM+ABOE+ABOP+ABOI+OTR),0.0),2)))
+        CONCAT('TOTAL:  $ ',FORMAT(IFNULL(SUM(CE+CT+CR+ENG+ABOT+ABOG+ABOM+ABOE+ABOP+ABOI+OTR),0.0)/1.16,2)))
 FROM tmp_ingresos_dia;
